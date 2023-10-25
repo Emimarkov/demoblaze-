55c5b07d98e6f509828ed63d8fe16f4eb9376e0b
Emiliano Markov <Bi@emilianomarkov.onmicrosoft.com>
2023-10-25 09:02:32 -0300

    Corregir error, no paso el otro commit.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  retries:1, // si falla que reintente de nuevo, eso es para validar que realmente fallo la prueba
   e2e: {
     setupNodeEvents(on, config) {
             allureWriter(on, config);
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random01'
+    const contrasena= 'random01'
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(usuario,contrasena)
+    cy.get('a#nameofuser').should('contains.text',usuario)
+    cy.wait(5000)
+
   })
 })
